2a481ce31b31161fc101b1997b126117fc9bec9c
eltonjtoledo <eltonjtoledo@gmail.com>
2019-02-20 17:25:20 -0300

    Complementando classe Phone.php

--- vendor/eltclass/php-classes/src/Pagseguro/Phone.php
+++ vendor/eltclass/php-classes/src/Pagseguro/Phone.php
@@ -5,4 +5,35 @@ namespace EltClass\Pagseguro;
 class Phone {
     private $areaCode;
     private $number;
+
+    public function __construct(int $areaCode, int $number)
+    {
+        if($areaCode < 11 || !$areaCode || $areaCode >99)
+        {
+            throw new Exception('Digite um DDD Valido.'); 
+        }
+
+        if(strlen($number) < 8 || !$number || strlen($number) >9)
+        {
+            throw new Exception('Digite um DDD Valido.'); 
+        }
+
+        $this->areaCode = $areaCode;
+        $this->number = $number;
+    }
+
+    public function getDOMElement():DOMElement
+    {
+        $dom = new \DOMDocument();
+        $fone = $dom->createElement('document');
+        $fone = $dom->appendChild($fone);
+
+        $areaCode = $dom->createElement('areaCode', $this->areaCode);
+        $areaCode = $fone->appendChild($areaCode);
+
+        $number = $dom->createElement('number', $this->number);
+        $number = $fone->appendChild($number);
+
+        return $fone;
+    }
 }
