b0eb99c72ff3518b13f951dad14dda32671f07ea
eltonjtoledo <eltonjtoledo@gmail.com>
2019-02-22 21:34:02 -0300

    Criando XML Shipping.php

--- vendor/eltclass/php-classes/src/Pagseguro/Shipping.php
+++ vendor/eltclass/php-classes/src/Pagseguro/Shipping.php
@@ -11,4 +11,37 @@ class Shipping {
     private $address;
     private $type;
     private $cost;
-}
+
+    public function __construct(bool $shippingAddressRequired = true, Address $address,
+     int $type, float $cost)
+    {
+       if($type < 1 || $type > 3)
+       {
+            throw new Exception("ERRO: Informe um tipo de de frete valido");
+       }
+
+       $this->shippingAddressRequired = $shippingAddressRequired;
+       $this->address = $address;
+       $this->type = $type;
+       $this->cost = $cost;
+    }
+
+    public function getDOMElement():DOMElement
+    {
+        $dom = new \DOMDocument();
+        $shipping = $dom->createElement('shipping');
+        $shipping = $dom->appendChild($shipping);
+
+        $address = $this->address->getDOMElement();
+        $address = $dom->importNode($address, true);
+        $address = $shipping->appendChild($address);
+
+        $type = $dom->createElement('type', $this->type);
+        $type = $shipping->appendChild($type);
+
+        $cost = $dom->createElement('cost', number_format($this->cost, 2, ".",""));
+        $cost = $shipping->appendChild($cost);
+        
+        return $shipping;
+    }
+} 
