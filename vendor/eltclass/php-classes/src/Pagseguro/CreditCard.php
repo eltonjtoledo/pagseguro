152b75bdb7718668c9c4284ac97dfe3aeea3785e
eltonjtoledo <eltonjtoledo@gmail.com>
2019-02-22 22:20:11 -0300

    Crindo XML creditCard

--- vendor/eltclass/php-classes/src/Pagseguro/CreditCard.php
+++ vendor/eltclass/php-classes/src/Pagseguro/CreditCard.php
@@ -7,4 +7,38 @@ class CreditCard {
     private $installment;
     private $holder;
     private $billingAddress;
+
+    public function __construct(string $token, Installment $installment, 
+    Holder $holder, Address $billingAddress)
+    {
+       if(!$token)
+       {
+        throw new Exception("ERRO: Informe o token do cartão");
+       }
+
+       $this->token = $token;
+       $this->installment = $installment;
+       $this->holder = $holder;
+       $this->billingAddress = $billingAddress;
+    }
+        
+    public function getDOMElement():DOMElement
+    {
+        $dom = new \DOMDocument();
+        $creditCard = $dom->createElement('creditCard');
+        $creditCard = $dom->appendChild($creditCard);
+
+        $token = $dom->createElement('token', $this->token);
+        $token = $creditCard->appendChild($token);
+    
+        $installment = $this->installment->getDOMElement();
+        $installment = $dom->importNode($installment, true);
+        $installment = $creditCard->appendChild($installment);
+    
+        $billingAddress = $this->billingAddress->getDOMElement('billingAddress');
+        $billingAddress = $dom->importNode($billingAddress, true);
+        $billingAddress = $creditCard->appendChild($billingAddress);
+
+        return $creditCard;
+    }
 }
